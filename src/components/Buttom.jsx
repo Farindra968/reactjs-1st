e4c2c4b7009da10efe9bb8715c0cd33479b4ae63
Farindra968 <farindrabahadurbhandari@gmail.com>
2024-09-21 14:09:05 +0545

    first commit

---
+++ src/components/Buttom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Buttom(props) {
+    console.log(Buttom);
+    
+  return (
+    <div>
+      <Buttom>Clck Me</Buttom>
+    </div>
+  )
+}
+
+export default Buttom
